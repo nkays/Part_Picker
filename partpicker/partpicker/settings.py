45b8a46b174c12ca54c45a38dc752b3b9e20025c
nkays <nelsonskays@gmail.com>
2026-02-15 20:00:44 -0500

    updated secret key settings

--- partpicker/partpicker/settings.py
+++ partpicker/partpicker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$_wp+a3@xy4f2_x_3pn^1q_$r89x@+n!gt6cd8g_*1eque7l7'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
