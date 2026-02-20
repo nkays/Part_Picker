38e9b5bc60978abd03babbe032f0823b6e992482
nkays <nelsonskays@gmail.com>
2026-02-19 20:39:36 -0500

    @

--- src/BuzzardBuilds/wsgi.py
+++ src/BuzzardBuilds/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuzzardBuilds.settings')
 
