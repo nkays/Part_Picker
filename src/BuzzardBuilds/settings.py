691ead002b4964fd9d489d4b176b25e437642d9b
nkays <nelsonskays@gmail.com>
2026-02-19 21:16:28 -0500

    cake

--- src/BuzzardBuilds/settings.py
+++ src/BuzzardBuilds/settings.py
@@ -172,11 +172,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+print(STATIC_URL)
 
 # send our static files here
 # locked files that do not change during runtime
 # external static file server
-STATIC_ROOT = BASE_DIR / "static_root"
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 
 
