c0feb4da0aa74ed4d5963df8b8d8d03cc32fe059
nkays <nelsonskays@gmail.com>
2026-02-19 23:23:13 -0500

    Update settings.py
    Manifest test

--- src/BuzzardBuilds/settings.py
+++ src/BuzzardBuilds/settings.py
@@ -193,7 +193,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
