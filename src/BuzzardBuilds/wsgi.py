5435e2eddfd42ca7f59cc649cb31fe09551c78af
nkays <nelsonskays@gmail.com>
2026-02-20 07:31:18 -0500

    Update wsgi.py

--- src/BuzzardBuilds/wsgi.py
+++ src/BuzzardBuilds/wsgi.py
@@ -8,16 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from pathlib import Path
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# STATIC_ROOT = BASE_DIR / "staticfiles/"
-
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuzzardBuilds.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/app/src/staticfiles", prefix=" /static/")
