fa710c5b840f9f6df42335c0fa4354b086075ded
nkays <nelsonskays@gmail.com>
2026-02-19 22:18:48 -0500

    ?

--- src/BuzzardBuilds/wsgi.py
+++ src/BuzzardBuilds/wsgi.py
@@ -11,6 +11,10 @@ import os
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / "staticfiles/"
+
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuzzardBuilds.settings')
