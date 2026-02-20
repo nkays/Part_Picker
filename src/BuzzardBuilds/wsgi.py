643b792a62b31c4d3be7b7334f292fea108c3e3b
nkays <nelsonskays@gmail.com>
2026-02-19 20:36:11 -0500

    !

--- src/BuzzardBuilds/wsgi.py
+++ src/BuzzardBuilds/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuzzardBuilds.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=STATIC_ROOT, max_age=31536000)
