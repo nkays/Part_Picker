a8840fc6ba026fe3d4721c7e4bdf43e02e8c1bec
nkays <nelsonskays@gmail.com>
2026-02-20 20:58:03 -0500

    2

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -4,7 +4,7 @@ set -e
 cd /code
 
 # Run startup management commands
-uv run python manage.py sendtestemail --admins
+# uv run python manage.py sendtestemail --admins
 uv run python manage.py migrate
 uv run python manage.py auto_admin
 
