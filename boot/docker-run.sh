0f063d0b0ea0d14d23a4ed2e341fdf6c951378bc
nkays <nelsonskays@gmail.com>
2026-02-20 21:00:44 -0500

    0

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -6,7 +6,7 @@ cd /code
 # Run startup management commands
 # uv run python manage.py sendtestemail --admins
 uv run python manage.py migrate
-uv run python manage.py auto_admin
+# uv run python manage.py auto_admin
 
 # Default port (Railway overrides this with PORT env var)
 RUNTIME_PORT=${PORT:-8080}
