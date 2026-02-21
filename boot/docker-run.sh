9ef4fa8b76b034be6c45241d3544f8fc885d0a8c
nkays <nelsonskays@gmail.com>
2026-02-20 20:28:27 -0500

    no export

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -9,7 +9,7 @@ uv run python manage.py migrate
 uv run python manage.py auto_admin
 
 # Default port (Railway overrides this with PORT env var)
-export RUNTIME_PORT=${PORT:-8080}
-export RUNTIME_HOST=${HOST:-0.0.0.0}
+RUNTIME_PORT=${PORT:-8080}
+RUNTIME_HOST=${HOST:-0.0.0.0}
 # Start Gunicorn
 exec uv run gunicorn BuzzardBuilds.wsgi:application --bind $RUNTIME_HOST:$RUNTIME_PORT
