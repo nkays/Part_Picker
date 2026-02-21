2d0cdeb8ced526def9f5f9313111f35b179c84ec
nkays <nelsonskays@gmail.com>
2026-02-20 21:20:51 -0500

    ??

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -6,7 +6,7 @@ cd /code
 uv run python manage.py migrate
 
 RUNTIME_PORT=${PORT:-8080}
-RUNTIME_HOST=0.0.0.0
+RUNTIME_HOST=&{HOST:-0.0.0.0} 
 
 exec uv run gunicorn BuzzardBuilds.wsgi:application \
     --bind ${RUNTIME_HOST}:${RUNTIME_PORT} \
