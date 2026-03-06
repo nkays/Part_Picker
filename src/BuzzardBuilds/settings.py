4c68c08c0bc47708fcfdedb0078500ec21d836f7
nkays <nelsonskays@gmail.com>
2026-03-06 17:43:17 -0500

    local postgres setup

--- src/BuzzardBuilds/settings.py
+++ src/BuzzardBuilds/settings.py
@@ -139,8 +139,12 @@ WSGI_APPLICATION = 'BuzzardBuilds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drone_parts_db',
+        'USER': 'drone_user',
+        'PASSWORD': 'fl1teT3st',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
